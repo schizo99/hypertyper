9a18d1f64e7ee44d779eea5536338dfd7b3fb173
Andreas Åman <andreas.aman@atg.se>
2024-08-20 15:18:10 +0200

    Changes

--- src/main.rs
+++ src/main.rs
@@ -17,13 +17,22 @@ use crossterm::{
 };
 use rand::Rng;
 
+const WIDTH: i32 = 20;
 struct Word {
     word: String,
-    x: u16,
-    y: u16,
+    x: i32,
+    y: i32,
     started: bool,
     enabled: bool,
     completed: bool,
+}
+
+struct Player {
+    name: String,
+    shields: i32,
+    level: i32,
+    score: i32,
+    screen_width: i32,
 }
 
 fn main() {
@@ -42,95 +51,136 @@ fn main() {
     execute!(io::stdout(), Clear(ClearType::All)).unwrap();
     // get size of terminal
     let size = size().unwrap();
-    let width = size.0;
-    let height = size.1;
+    let width = size.0 as i32;
+    let height = size.1 as i32;
     let (tx, rx) = mpsc::channel();
     thread::spawn(move || loop {
         let key = get_key();
         tx.send(key).unwrap();
     });
-    fun_name(rx, height, width);
+    let player = Player {
+        name: "kalle".to_string(),
+        shields: 3,
+        level: 1,
+        score: 0,
+        screen_width: width,
+    };
+    fun_name(rx, height, player);
     execute!(io::stdout(), Show).unwrap();
     disable_raw_mode().unwrap();
 }
 
-fn draw_shield(width: u16, height: u16) {
-    for y in 0..height {
-        execute!(io::stdout(), MoveTo(width, y)).unwrap();
+fn draw_shield(width: i32, height: i32) {
+    for y in 1..height {
+        execute!(io::stdout(), MoveTo(width as u16, y as u16)).unwrap();
         print!("#");
     }
 }
 
-fn fun_name(rx: mpsc::Receiver<String>, height: u16, width: u16) {
+fn draw_toolbar(player: &Player) {
+    execute!(io::stdout(), MoveTo(0, 0)).unwrap();
+    print!("Score: {}", player.score);
+    execute!(io::stdout(), MoveTo(30, 0)).unwrap();
+    print!("Level: {}", player.level);
+    execute!(io::stdout(), MoveTo(60, 0)).unwrap();
+    print!("Shields: {}", player.shields);
+}
+
+fn randomword(width: i32, height: i32) -> Word {
+    let words = ["terminal", "rust", "bajs"];
+    let word = words[rand::thread_rng().gen_range(0..words.len())];
+    Word {
+        word: word.to_string(),
+        x: width - word.len() as i32,
+        y: rand::thread_rng().gen_range(0..height),
+        started: false,
+        enabled: false,
+        completed: false,
+    }
+}
+
+fn fun_name(rx: mpsc::Receiver<String>, height: i32, mut player: Player) {
     let mut winner = false;
 
-    let sleeptime = Duration::from_millis(100);
+    let sleeptime = Duration::from_millis(100 - player.level as u64);
     execute!(io::stdout(), Clear(ClearType::All)).unwrap();
-    let mut x_position = 0;
-    // let mut modify = word.to_string();
-    let mut counter = 0;
-    // let mut length = word.len() - counter;
     let mut words: Vec<Word> = Vec::new();
-    for w in ["terminal", "rust"] {
-        let word = Word {
-            word: w.to_string(),
-            x: x_position,
-            y: rand::thread_rng().gen_range(0..height),
-            started: false,
-            enabled: false,
-            completed: false,
-        };
-        words.push(word);
-    }
-    draw_shield(80, height);
+    // for w in ["terminal", "rust", "bajs"] {
+    //     let word = Word {
+    //         word: w.to_string(),
+    //         x: 1,
+    //         y: rand::thread_rng().gen_range(1..height),
+    //         started: false,
+    //         enabled: false,
+    //         completed: false,
+    //     };
+    //     words.push(word);
+    // }
+    draw_shield(WIDTH, height);
     let mut breakout = false;
-    let mut level = 1;
     loop {
+        draw_toolbar(&player);
         words.retain(|w| !w.completed);
-        if words.len() == 0 {
-            winner = true;
-            break;
+        if words.len() < 5 + player.level as usize {
+            let new_word = randomword(player.screen_width,height);
+            let conflict = words
+                .iter()
+                .any(|w| w.word.starts_with(&new_word.word[0..1]));
+            if !conflict {
+                words.push(new_word);
+            }
         }
         for w in words.iter_mut() {
-            if !w.started {
+            if !w.enabled {
                 if rand::thread_rng().gen_range(0..100) < 10 {
                     w.enabled = true;
                 }
             }
-        }
-        for w in words.iter_mut() {
-            if w.enabled && !w.completed {
-                execute!(io::stdout(), MoveTo(80, w.y)).unwrap();
-                execute!(io::stdout(), Clear(ClearType::CurrentLine)).unwrap();
-                let a = format!(
-                    "# {:>width$}",
-                    &w.word[0..],
-                    width = width as usize - 82 as usize - counter as usize
-                );
-                execute!(io::stdout(), Print(a)).unwrap();
-                counter += 1;
-                match rx.try_recv() {
-                    Ok(key) => {
-                        if key == "\x1B" {
-                            breakout = true;
-                        }
-                        if w.word.starts_with(&key) {
-                            w.started = true;
-                            w.word = w.word[1..].to_string();
-                            if w.word.len() == 0 {
-                                w.completed = true;
-                            }
-                        }
-                    }
-                    Err(_) => {}
+            if w.x < WIDTH {
+                player.shields -= 1;
+                if player.shields == 0 {
+                    winner = false;
+                    breakout = true;
                 }
+                w.completed = true;
             }
         }
-        sleep(sleeptime);
-        if counter > width as usize - 82 {
-            winner = false;
-            break;
+        let mut keypressed = "kalle".to_string();
+        match rx.try_recv() {
+            Ok(key) => {
+                if key == "\x1B" {
+                    breakout = true;
+                }
+                keypressed = key;
+            }
+            Err(_) => {}
         }
+
+        let any_started = words.iter().any(|w| w.started);
+
+        for w in words.iter_mut() {
+            if w.enabled && !w.completed {
+                w.x -= 1;
+                if w.word.starts_with(&keypressed) && w.started {
+                    w.word = w.word[1..].to_string();
+                } else if !w.started && !any_started {
+                    if w.word.starts_with(&keypressed) {
+                        w.started = true;
+                        w.word = w.word[1..].to_string();
+                    }
+                }
+                execute!(io::stdout(), MoveTo(WIDTH as u16, w.y as u16)).unwrap();
+                execute!(io::stdout(), Clear(ClearType::CurrentLine)).unwrap();
+                execute!(io::stdout(), Print(format!("#"))).unwrap();
+                execute!(io::stdout(), MoveTo(w.x as u16, w.y as u16)).unwrap();
+                execute!(io::stdout(), Print(format!("{}", &w.word))).unwrap();
+            }
+            if w.word.len() == 0 {
+                w.completed = true;
+            }
+        }
+
+        sleep(sleeptime);
         if breakout {
             break;
         }
